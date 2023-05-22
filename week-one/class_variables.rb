ae8cecb8f54c4d001b71d251dcc6480b25addf41
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2023-05-22 09:37:05 +0300

    class varibles & methods

---
+++ week-one/class_variables.rb
@@ -0,0 +1,37 @@
+# Local variables - name
+# Global - $name
+# Instance - @name
+# CLass - @@name
+
+class Dog
+  # Captures info related to specific instances of this class
+  @@good_dogs = 0
+
+  # Constants - variables that do not change
+  # Using uppercase with snake case naming convention
+  NUMBER_OF_LEGS = 4
+
+  attr_accessor :legs
+
+  def initialize
+    @@good_dogs += 1
+    self.legs = NUMBER_OF_LEGS
+  end
+
+  # class method
+  # must start with self.
+  def self.number_of_good_dogs
+    @@good_dogs
+  end
+end
+
+p Dog.number_of_good_dogs
+
+dog_one = Dog.new
+dog_two = Dog.new
+
+p "Dog one #{dog_one.legs}"
+
+p "Dog two #{dog_two.legs}"
+
+p Dog.number_of_good_dogs
